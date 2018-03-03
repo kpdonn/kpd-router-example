8db7dfc6e6b69a9109a0fe2dbe9732c44f77456b
Kevin Donnelly <kdonnelly08@gmail.com>
2018-03-03 02:46:19 -0600

    Add prettier.

--- src/routerConfig.tsx
+++ src/routerConfig.tsx
@@ -1,22 +1,11 @@
-import {newRouter} from 'kpd-router'
+import { newRouter } from "kpd-router"
 
+import { Main } from "./Main"
+import { Other } from "./Other"
 
-
-import { Main } from './Main';
-import { Other } from './Other';
-
-
-
-export const router = newRouter().addRoute({
-    name: 'main',
-    path: '/',
-    component: Main
-  }).addRoute({
-      name: 'other',
-      path: "/other",
-      component: Other
-  })
+export const router = newRouter()
+  .addRoute({ name: "main", path: "/", component: Main })
+  .addRoute({ name: "other", path: "/other", component: Other })
   .start()
-
 
 export const Link = router.Link
